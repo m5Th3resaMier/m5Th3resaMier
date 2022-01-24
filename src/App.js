b6324aa18bb7b674c7f03537070ba8f3d7f3c931
matheresamier <78998859+matheresamier@users.noreply.github.com>
2022-01-24 16:58:11 +0800

    Login and Registration UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import LoginForm from './components/LoginForm';
+import LoginForm1 from './components/LoginForm1';
+import RegistrationForm from './components/RegistrationForm';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm1 />
+      {/* <RegistrationForm /> */}
+     
+    </div>
+  );
+}
+
+export default App;
